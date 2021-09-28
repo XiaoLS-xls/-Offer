7b55449d7cf2267b3991f4c155ade5080bcf1d8d
XiaoLS-xls <74584911+XiaoLS-xls@users.noreply.github.com>
2021-09-28 10:20:41 +0800

    Add files via upload

---
+++ 剑指 Offer 24. 反转链表.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        //迭代
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
+    //也可以使用递归
