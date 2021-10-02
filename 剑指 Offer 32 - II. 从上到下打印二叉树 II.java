42d47fea4897c84ca0b79d75bf05be69144e4484
XiaoLS-xls <74584911+XiaoLS-xls@users.noreply.github.com>
2021-10-02 16:26:05 +0800

    多了记录层次的个数（直接for循环）

---
+++ 剑指 Offer 32 - II. 从上到下打印二叉树 II.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        if(root != null) queue.add(root);
+        while(!queue.isEmpty()) {
+            List<Integer> tmp = new ArrayList<>();
+            for(int i = queue.size(); i > 0; i--) {         //非常漂亮的写法，定义的i为queue.size,直接将队中的元素全部出队，（保留了该层的个数）
+                TreeNode node = queue.poll();
+                tmp.add(node.val);
+                if(node.left != null) queue.add(node.left);
+                if(node.right != null) queue.add(node.right);
+            }
+            res.add(tmp);
+        }
+        return res;
+    }
+}
+
